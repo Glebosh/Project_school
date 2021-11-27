adda8e87355ccc3a05bbb707f75b1d43c7d1d05b
Glebosh <glebosh@gmail.com>
2021-11-27 21:48:12 +0300

    hostsite

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python hosting.py
---
+++ hosting.py
@@ -0,0 +1,11 @@
+import http.server
+import socketserver
+
+PORT = 8000
+
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("0.0.0.0", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
+
