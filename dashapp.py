bdc51957b77dd97c6354adc9a5e65d5884f3fe29
Glebosh <glebosh@gmail.com>
2021-12-04 23:08:51 +0300

    Dash graph_m, subj

--- dashapp.py
+++ dashapp.py
@@ -5,22 +5,26 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.express as px
+import functions as func
 
 import pandas as pd
+
+data = pd.read_html('Отчет об успеваемости и посещаемости ученика.xls')
+
+df = data[1]
 
 app = dash.Dash(__name__)
 
 # assume you have a "long-form" data frame
 # see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
+#df = pd.DataFrame({
+#    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+#    "Amount": [4, 1, 2, 2, 4, 5],
+#    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+#})
 
-fig_one = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
-fig_two = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group", color_discrete_sequence=px.colors.sequential.Viridis)
-
+fig_one = func.plot_marks(df)
+fig_two = func.plot_subject(df, 'Алгебра')
 # <div class="row">
 #   <div class="column"></div>
 #   <div class="column"></div>
@@ -32,13 +36,13 @@ app.layout = html.Div(
         html.Div(
             className="column",
             children=dcc.Graph(
-                id='example-graph-one',
+                id='graph_months',
                 figure=fig_one)
         ),
         html.Div(
             className="column",
             children=dcc.Graph(
-                id='example-graph-two',
+                id='graph_subject',
                 figure=fig_two)
         ),   
 ])
